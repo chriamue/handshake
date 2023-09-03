f908dda689976029665c569294cdfb7fd4e80af8
Christian M <chriamue@gmail.com>
2023-09-03 09:27:03 +0200

    make signup payable

--- contract/lib.rs
+++ contract/lib.rs
@@ -1,10 +1,10 @@
 #![cfg_attr(not(feature = "std"), no_std, no_main)]
 
 mod azns_router {
+    use openbrush::traits::AccountId;
     use scale_info::prelude::string::String;
     use scale_info::prelude::vec::Vec;
-    use openbrush::traits::AccountId;
-    
+
     #[derive(Debug, PartialEq, Eq, scale::Encode, scale::Decode)]
     #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
     pub enum Error {
@@ -26,13 +26,13 @@ mod azns_router {
     pub trait AznsContract {
         #[ink(message, selector = 0xe6da7bf0)]
         fn get_all_registries(&self) -> Vec<AccountId>;
-     
+
         #[ink(message, selector = 0x15a5d20a)]
         fn get_registry(&self, tld: String) -> Option<AccountId>;
-     
+
         #[ink(message, selector = 0xd259f7ba)]
         fn get_address(&self, domain: String) -> Result<AccountId, Error>;
-     
+
         #[ink(message, selector = 0xdf3a358e)]
         fn get_primary_domains(
             &self,
@@ -48,10 +48,7 @@ pub mod handshake {
     use openbrush::traits::Storage;
     use openbrush::traits::String;
 
-    use crate::azns_router::{
-        AznsContract,
-        Error as AznsRouterError,
-    };
+    use crate::azns_router::{AznsContract, Error as AznsRouterError};
 
     #[ink(storage)]
     #[derive(Default, Storage)]
@@ -59,6 +56,7 @@ pub mod handshake {
         #[storage_field]
         psp34: psp34::Data,
         next_id: u8,
+        ticket_price: Balance,
         accounts: ink::prelude::vec::Vec<AccountId>,
         #[storage_field]
         handshakes: ink::prelude::vec::Vec<(AccountId, AccountId)>,
@@ -66,12 +64,19 @@ pub mod handshake {
 
     impl Handshake {
         #[ink(constructor)]
-        pub fn new() -> Self {
-            Self::default()
+        pub fn new(ticket_price: Balance) -> Self {
+            Self {
+                ticket_price,
+                ..Default::default()
+            }
         }
 
-        #[ink(message)]
+        #[ink(message, payable)]
         pub fn signup(&mut self) -> Result<(), PSP34Error> {
+            assert!(
+                self.ticket_price <= Self::env().transferred_value(),
+                "payment was not the ticket price"
+            );
             let caller = Self::env().caller();
             psp34::Internal::_mint_to(self, caller, Id::U8(self.next_id))?;
             self.next_id += 1;
@@ -116,7 +121,7 @@ pub mod handshake {
             tld: Option<String>,
         ) -> Vec<(AccountId, String)> {
             let router: ink::contract_ref!(AznsContract) = router_addr.into();
-        
+
             router.get_primary_domains(account, tld)
         }
     }
@@ -125,10 +130,10 @@ pub mod handshake {
     mod tests {
         use super::*;
         #[ink::test]
-        fn total_supply_works() {
-            let token = Handshake::new();
-            assert_eq!(token.num_accounts(), Ok(0));
-            assert_eq!(token.num_handshakes(), Ok(0));
+        fn new_contract_works() {
+            let contract = Handshake::new(7);
+            assert_eq!(contract.num_accounts(), Ok(0));
+            assert_eq!(contract.num_handshakes(), Ok(0));
         }
     }
 
@@ -139,7 +144,6 @@ pub mod handshake {
         use ink_e2e::subxt::tx::Signer;
         use ink_e2e::subxt::utils::AccountId32;
         use ink_e2e::{build_message, Keypair, PolkadotConfig};
-        use openbrush::contracts::psp34::psp34_external::PSP34;
 
         type ContractRef = HandshakeRef;
 
@@ -150,23 +154,9 @@ pub mod handshake {
             address.0.into()
         }
 
-        async fn balance_of(
-            client: &mut ink_e2e::Client<PolkadotConfig, ink_env::DefaultEnvironment>,
-            address: ink::primitives::AccountId,
-            account: ink::primitives::AccountId,
-        ) -> Balance {
-            let _msg = build_message::<ContractRef>(address.clone())
-                .call(|contract| contract.balance_of(account));
-            let result = client
-                .call_dry_run(&ink_e2e::alice(), &_msg, 0, None)
-                .await
-                .return_value();
-            result.into()
-        }
-
         #[ink_e2e::test]
         async fn account_signup(mut client: ink_e2e::Client<C, E>) -> E2EResult<()> {
-            let constructor = ContractRef::new();
+            let constructor = ContractRef::new(7);
             let address = client
                 .instantiate("handshake", &ink_e2e::alice(), constructor, 0, None)
                 .await
@@ -177,7 +167,7 @@ pub mod handshake {
                 let _msg = build_message::<ContractRef>(address.clone())
                     .call(|contract| contract.signup());
                 client
-                    .call(&ink_e2e::alice(), _msg, 0, None)
+                    .call(&ink_e2e::alice(), _msg, 7, None)
                     .await
                     .expect("signup failed")
             };
@@ -200,7 +190,7 @@ pub mod handshake {
 
         #[ink_e2e::test]
         async fn count_handshakes(mut client: ink_e2e::Client<C, E>) -> E2EResult<()> {
-            let constructor = ContractRef::new();
+            let constructor = ContractRef::new(7);
             let address = client
                 .instantiate("handshake", &ink_e2e::alice(), constructor, 0, None)
                 .await
@@ -228,6 +218,31 @@ pub mod handshake {
             };
 
             assert_eq!(num_accounts.unwrap(), 1, "Should have 1 account");
+
+            Ok(())
+        }
+
+        #[ink_e2e::test]
+        async fn cannot_signup_without_payment(mut client: ink_e2e::Client<C, E>) -> E2EResult<()> {
+            let constructor = ContractRef::new(7);
+            let address = client
+                .instantiate("handshake", &ink_e2e::alice(), constructor, 0, None)
+                .await
+                .expect("instantiate failed")
+                .account_id;
+
+            let result = {
+                let _msg = build_message::<ContractRef>(address.clone())
+                    .call(|contract| contract.signup());
+                client.call(&ink_e2e::alice(), _msg, 0, None).await
+            };
+
+            if let Err(ink_e2e::Error::CallDryRun(dry_run)) = result {
+                let debug_message = String::from_utf8_lossy(&dry_run.debug_message);
+                assert!(debug_message.contains("payment was not the ticket price"))
+            } else {
+                panic!("Signup without payment should fail")
+            }
 
             Ok(())
         }
